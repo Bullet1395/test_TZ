12309239f3bbc03399e8f92f6cac05e5a5224973
Bullet1395 <badmax666@list.ru>
2018-06-23 17:20:10 +0700

    start

--- test_TZ/Form1.Designer.cs
+++ test_TZ/Form1.Designer.cs
@@ -28,21 +28,165 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.файлToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.сохранитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.checkClick = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.openFileXML = new System.Windows.Forms.OpenFileDialog();
+            this.saveFileXML = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.файлToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(646, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // файлToolStripMenuItem
+            // 
+            this.файлToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.сохранитьToolStripMenuItem,
+            this.загрузитьToolStripMenuItem});
+            this.файлToolStripMenuItem.Name = "файлToolStripMenuItem";
+            this.файлToolStripMenuItem.Size = new System.Drawing.Size(48, 20);
+            this.файлToolStripMenuItem.Text = "Файл";
+            // 
+            // сохранитьToolStripMenuItem
+            // 
+            this.сохранитьToolStripMenuItem.Name = "сохранитьToolStripMenuItem";
+            this.сохранитьToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.сохранитьToolStripMenuItem.Text = "Сохранить ";
+            this.сохранитьToolStripMenuItem.Click += new System.EventHandler(this.сохранитьToolStripMenuItem_Click);
+            // 
+            // загрузитьToolStripMenuItem
+            // 
+            this.загрузитьToolStripMenuItem.Name = "загрузитьToolStripMenuItem";
+            this.загрузитьToolStripMenuItem.Size = new System.Drawing.Size(135, 22);
+            this.загрузитьToolStripMenuItem.Text = "Загрузить";
+            this.загрузитьToolStripMenuItem.Click += new System.EventHandler(this.загрузитьToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(12, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(463, 362);
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkClick);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(481, 56);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(161, 68);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Проверка по щелчку";
+            // 
+            // checkClick
+            // 
+            this.checkClick.AutoSize = true;
+            this.checkClick.Location = new System.Drawing.Point(6, 19);
+            this.checkClick.Name = "checkClick";
+            this.checkClick.Size = new System.Drawing.Size(126, 17);
+            this.checkClick.TabIndex = 1;
+            this.checkClick.Text = "Вкл/выкл проверку";
+            this.checkClick.UseVisualStyleBackColor = true;
+            this.checkClick.CheckedChanged += new System.EventHandler(this.checkClick_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 39);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Не активно";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(481, 27);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(161, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Создать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(481, 366);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(161, 23);
+            this.clearButton.TabIndex = 4;
+            this.clearButton.Text = "Очистить";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // openFileXML
+            // 
+            this.openFileXML.FileName = "Выберите XML-файл";
+            this.openFileXML.Title = "Открытие файла";
+            // 
+            // saveFileXML
+            // 
+            this.saveFileXML.FileName = "Выберите место сохранения и имя файла";
+            this.saveFileXML.Title = "Сохранение файла";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(477, 347);
+            this.ClientSize = new System.Drawing.Size(646, 401);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem файлToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьToolStripMenuItem;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkClick;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.OpenFileDialog openFileXML;
+        private System.Windows.Forms.SaveFileDialog saveFileXML;
     }
 }
 
--- test_TZ/Form1.cs
+++ test_TZ/Form1.cs
@@ -3,18 +3,168 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace test_TZ
 {
     public partial class Form1 : Form
     {
+        List<Point> pointsAll;
+
         public Form1()
         {
             InitializeComponent();
+            pointsAll = new List<Point>();
+
+        }
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (checkClick.Checked == false)
+            {
+                pointsAll.Add(new Point(e.X, e.Y));
+
+                pictureBox1.CreateGraphics().FillRectangle(Brushes.Black, e.X, e.Y, 2, 2);
+
+                pictureBox1.CreateGraphics().Dispose();
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (pointsAll.Count != 0)
+            {
+                paintsOnPicture(pointsAll);
+            }
+            else
+            {
+                MessageBox.Show("Нет доступных точек для рисования, возможно вы находитесь в режиме проверки.", "Обратите внимание", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        public void paintsOnPicture(List<Point> pointsForPaint)
+        {
+            Point[] pointsArray = new Point[pointsForPaint.Count];
+            for (int i = 0; i < pointsForPaint.Count; i++)
+            {
+                pointsArray[i] = pointsForPaint[i];
+            }
+
+            pictureBox1.CreateGraphics().DrawPolygon(Pens.Black, pointsArray);
+            pictureBox1.CreateGraphics().FillPolygon(Brushes.LightGray, pointsArray);
+        }
+
+        private void checkClick_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkClick.Checked == false)
+            {
+                label1.Text = "Не активно";
+            }
+            else
+            {
+                label1.Text = "";
+            }
+        }
+
+        public bool isCrossLine(Point p1, Point p2, Point p3, Point p4)
+        {
+            long x;
+            long y;
+
+            long dx1 = p2.X - p1.X;
+            long dy1 = p2.Y - p1.Y;
+            long dx2 = p4.X - p3.X;
+            long dy2 = p4.Y - p3.Y;
+
+            x = dy1 * dx2 - dy2 * dx1;
+
+            if (x == 0 || dx2 == 0)
+                return
+                    false;
+
+            y = p3.X * p4.Y - p3.Y * p4.X;
+            x = ((p1.X * p2.Y - p1.Y * p2.X) * dx2 - y * dx1) / x;
+            y = (dy2 * x - y) / dx2;
+
+            return
+                ((p1.X <= x && p2.X >= x) || 
+                (p2.X <= x && p1.X >= x)) && 
+                ((p3.X <= x && p4.X >= x) || 
+                (p4.X <= x && p3.X >= x));
+
+        }
+
+        public bool checkPointInPolygon(List<Point> points, Point point)
+        {
+            int checkForParity = 0;
+
+            for (int i = 0; i < points.Count; i++)
+            {
+                if (i == points.Count - 1)
+                {
+                    if (isCrossLine(points[i], points[0], point, new Point(pictureBox1.Size.Width, point.Y)))
+                        checkForParity++;
+                }
+                else if (isCrossLine(points[i], points[i + 1], point, new Point(pictureBox1.Size.Width, point.Y)))
+                    checkForParity++;
+            }
+
+            if (checkForParity % 2 == 0)
+                return false;
+            return true;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (checkClick.Checked == true)
+            {
+                if (checkPointInPolygon(pointsAll, new Point(e.X, e.Y)))
+                    label1.Text = "Внутри полигона";
+                else
+                    label1.Text = "Снаружи полигона";
+            }
+        }
+
+        private void сохранитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileXML.Filter = "XML файлы (*.xml) |*.xml";
+
+            if (saveFileXML.ShowDialog() == DialogResult.OK)
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(List<Point>));
+
+                Stream writer = new FileStream(saveFileXML.FileName, FileMode.Create);
+                xs.Serialize(writer, pointsAll);
+                writer.Close();
+            }
+        }
+
+        private void загрузитьToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileXML.Filter = "XML файлы (*.xml) |*.xml|Все файлы |*.*";
+
+            if (openFileXML.ShowDialog() == DialogResult.OK)
+            {
+                XmlSerializer xs = new XmlSerializer(typeof(List<Point>));
+
+                Stream reader = new FileStream(openFileXML.FileName, FileMode.Open);
+                pointsAll = (List<Point>)xs.Deserialize(reader);
+                reader.Close();
+
+                paintsOnPicture(pointsAll);
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            pointsAll.Clear();
+            pictureBox1.Image = null;
         }
     }
 }
